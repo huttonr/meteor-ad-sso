07214967622b8243bc5c21cf02795a5e42ea6f9c
HuttonR <hutton.rounds@thebronson.com>
2015-12-23 01:46:59 -0800

    Major rewrite, nixed Blaze and iron:router

---
+++ adsso/adsso-client.js
@@ -0,0 +1,43 @@
+SSO = {};
+
+var authId = Meteor.uuid();
+//var timesRefreshed = new ReactiveVar();
+
+Meteor.startup(function () {
+  var env = __meteor_runtime_config__.ROOT_URL;
+  var isProd = env.indexOf('localhost') === -1 && env.indexOf('127.0') === -1;
+
+  var src = (isProd ? SSO.authUrl + SSO.authApp : SSO.devAuthUrl + SSO.devAuthApp) + "/" + authId;
+
+  //timesRefreshed.set(0);
+
+  var $iframe = $('<iframe></iframe>')
+    .attr('style', 'display: none')
+    .attr('src', src)
+    .load(function () {
+      if (Meteor.user()) return;
+      SSO.login();
+    });
+
+  $('body').append($iframe);
+});
+
+
+SSO.login = function () {
+  Accounts.callLoginMethod({
+    methodArguments: [
+      { authId : authId }
+    ],
+    userCallback : function (err, res) {
+      if (err) {
+        if (typeof SSO.onError === "function")  {
+          SSO.onError(err);
+        } else {
+          console.error(err);
+        }
+      } else {
+        if (typeof SSO.onSuccess === "function") SSO.onSuccess(res);
+      }
+    }
+  });
+}
